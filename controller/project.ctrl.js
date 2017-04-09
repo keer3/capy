b7e973b075fb88af1a80554c7b80741832550007
sunshumin <ssmin3@163.com>
2017-04-09 14:09:38 +0800

    查看项目协作者

--- controller/project.ctrl.js
+++ controller/project.ctrl.js
@@ -3,6 +3,7 @@ const Response = require('../utils/Response')
 const Util = require('util')
 
 const ProjectModel = Sequelize.import('../models/project.model')
+const UserModel = Sequelize.import('../models/user.model')
 const ProjectUserModel = Sequelize.import('../models/projectUser.model')
 
 // 查询用户拥有的项目
@@ -49,6 +50,50 @@ const findProjectListByUser = async(req, res) => {
 
 }
 
+// 查询项目协作者
+const findUserListByProject = async(req, res) => {
+  try {
+    req.checkQuery('projectId', '项目ID不能为空').notEmpty()
+
+    // 检查参数
+    const result = await req.getValidationResult()
+    if (!result.isEmpty()) {
+      Response.error(res, 500, Util.inspect(result.array()))
+      return
+    }
+
+    const projectId = req.query.projectId
+    const userProjectList = await ProjectUserModel.findAll({
+      where: {
+        project_id: projectId
+      }
+    })
+
+    var userIdList = []
+    for (var userProject of userProjectList) {
+      userIdList.push(userProject.user_id)
+    }
+
+    const userList = await UserModel.findAll({
+      attributes: ['username', 'phone', 'realname', 'email'],
+      where: {
+        id: {
+          $in: userIdList
+        }
+      }
+    })
+
+    if (!userList) {
+      Response.error(res, 500, '没有用户')
+      return
+    }
+    Response.success(res, userList)
+  } catch (error) {
+    Response.error(res, 500, error)
+  }
+}
+
 module.exports = {
-  findProjectListByUser
+  findProjectListByUser,
+  findUserListByProject
 }
--- router/project.js
+++ router/project.js
@@ -5,4 +5,6 @@ const ProjectCtrl = require('../controller/project.ctrl')
 
 router.get('/findProjectByUser', ProjectCtrl.findProjectListByUser)
 
+router.get('/findUserByProject', ProjectCtrl.findUserListByProject)
+
 module.exports = router
